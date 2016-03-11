ff358e5ede9710103a7f72bde28dedef2db3f5ac
Valentin Radu <valentin.radu@endava.com>
2016-03-11 10:34:25 +0200

    Initial Commit

---
+++ Conveyor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Conveyor
+//
+//  Created by Valentin Radu on 11/03/16.
+//  Copyright © 2016 Valentin Radu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
